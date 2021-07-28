d84c4011ce0fb7bc0a0b5b19d83f8c7d18712f57
yoshihiko555 <yoshihiko05410@gmail.com>
2021-07-28 22:52:36 +0900

    change README

--- next-blog/README.md
+++ next-blog/README.md
@@ -1,1 +1,3 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+[sample page](https://next-offcial-tutorial-per3jyz72-yoshihiko555.vercel.app/)
--- next-blog/pages/posts/[id].js
+++ next-blog/pages/posts/[id].js
@@ -27,6 +27,13 @@ export async function getStaticPaths () {
     // idとして取りうる値の配列は、returnされたオブジェクトのpathsキーに対応する値でなければならない
     paths,
     // fallback: falseであれば、getStaticPathsからreturnされていないあらゆるパスは404になる
+    // 逆にtrueを設定した場合、pathsに指定されていないパスからのアクセスがあったとしても、404にならない
+    // 代わりに以下のようなフローで新たに性的ファイルを生成し、クライアント側に返却する
+    // 1. /posts/3のパスがRequestとしてくる
+    // ※pathsに定義されていないidであるが、fallback: trueなので404ページは返却しない
+    // 2. 裏側でサーバ側がgetStaticPropsを実行し、id=3に紐づく静的なファイルを生成する
+    // 3. 生成が完了したら作成した静的ファイルを返す
+    // 4. これ以降の/posts/3は②で作成した静的ファイルを返す。
     fallback: false
   }
 }
